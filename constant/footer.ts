a88ce09d34597193aa9ca866d3e7c80e1d383b6d
tariqul420 <tariqul.developer@gmail.com>
2025-06-17 10:24:01 +0600

    feat: add layout, footer, and navbar components with responsive design
    - Implemented a new layout component to wrap application pages.
    - Created a footer component with useful links, categories, and social media icons.
    - Developed a navbar component with dynamic links based on user authentication status.
    - Integrated Radix UI for dropdown and sheet components for better UI interactions.
    - Added utility functions for managing class names and styles.
    - Updated package.json to include new dependencies for Radix UI components.
    - Defined constants for footer links and categories for better maintainability.

---
+++ constant/footer.ts
@@ -0,0 +1,57 @@
+export const usefulLinks = [
+  {
+    name: 'About Us',
+    path: '/about-us',
+  },
+  {
+    name: 'Contact Us',
+    path: '/contact-us',
+  },
+  {
+    name: 'Returns and Refunds Policy',
+    path: '/returns-and-refunds-policy',
+  },
+  {
+    name: 'Terms and Conditions',
+    path: '/terms-and-conditions',
+  },
+];
+
+export const categories = [
+  {
+    name: 'Digestive Health',
+    path: '/products?category=digestive-health',
+  },
+  {
+    name: 'Skin & Hair Care',
+    path: '/products?category=skin-hair-care',
+  },
+  {
+    name: 'Respiratory Health',
+    path: '/products?category=respiratory-health&sort=default',
+  },
+  {
+    name: 'Metabolic Health',
+    path: '/products?category=metabolic-health',
+  },
+];
+
+export const quickLinks = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Products',
+    path: '/products',
+  },
+  {
+    name: 'Blogs',
+    path: '/blogs',
+  },
+
+  {
+    name: 'Login / Register',
+    path: '/sign-in',
+  },
+];
